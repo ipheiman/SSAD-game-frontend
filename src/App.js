040e4964244b343986871ee240b1ab514855cf94
ipheiman <ipheiman@gmail.com>
2020-03-19 22:18:48 +0800

    Added 10 test cases

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ class App extends Component {
     return (
       <div>
         <World />
-        {/* <ReactPlayer
+        <ReactPlayer
+          className="react-player"
           width="0%"
           height="0%"
           url="https://www.youtube.com/watch?v=5O3a5opHbY4&t=18s"
           playing
-        /> */}
+        />
       </div>
     );
   }
--- src/config/constants.js
+++ src/config/constants.js
@@ -1,8 +1,6 @@
 export const SPRITE_SIZE = 40;
-// export const MAP_HEIGHT = SPRITE_SIZE * 12;
-// export const MAP_WIDTH = SPRITE_SIZE * 20;
 export const MAP_HEIGHT = SPRITE_SIZE * 16;
 export const MAP_WIDTH = SPRITE_SIZE * 30;
 
 //get list of obstacle coords from backend
-export const obstacle = [1,6] //snorlax tile=7
+export const obstacle = [1, 6]; //snorlax tile=7
