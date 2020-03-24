035f96790cd0aca65cf4ea3e72eee0a291d54e1e
gitteroy <elroyliangbojun@gmail.com>
2020-03-24 15:43:55 +0800

    nothing_much

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import World from "./features/world";
-import ReactPlayer from "react-player";
+// import ReactPlayer from "react-player";
 
 class App extends Component {
   render() {
     return (
       <div>
         <World />
-        <ReactPlayer
+        {/* <ReactPlayer
           className="react-player"
           width="0%"
           height="0%"
           url="https://www.youtube.com/watch?v=5O3a5opHbY4&t=18s"
           playing
-        />
+        /> */}
       </div>
     );
   }
